94b436fa60b4ce9e1da117d2dea56166d23e3812
scraemon <88232177+scraemon@users.noreply.github.com>
2022-11-22 14:29:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,3 +32,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add')
+my_cur.execute(f'insert into fruit_load_list values ({add_my_fruit})')
+streamlit.write(f'Thanks for adding {add_my_fruit}')
